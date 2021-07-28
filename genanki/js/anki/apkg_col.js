ca97befbbc41d9832b8369a9aa00ce703d3c798a
Mani <12841290+infinyte7@users.noreply.github.com>
2021-07-29 06:52:44 +0800

    anki schema and col for db

---
+++ genanki/js/anki/apkg_col.js
@@ -0,0 +1,138 @@
+/**
+ * This file is transliterated into JavaScript from genanki's Python version,
+ * available here:
+ * https://github.com/kerrickstaley/genanki
+ *
+ * The original is licensed as MIT, and so is this version (though the rest of
+ * mkanki is licensed AGPLv3).
+ *
+ * The following license applies to this file only.
+ *
+ * Copyright (c) 2017 Kerrick Staley and Jeremy Apthorp.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+var APKG_COL = `
+INSERT INTO col VALUES(
+    null,
+    1411124400,
+    1425279151694,
+    1425279151690,
+    11,
+    0,
+    0,
+    0,
+    '{
+        "activeDecks": [
+            1
+        ],
+        "addToCur": true,
+        "collapseTime": 1200,
+        "curDeck": 1,
+        "curModel": "1425279151691",
+        "dueCounts": true,
+        "estTimes": true,
+        "newBury": true,
+        "newSpread": 0,
+        "nextPos": 1,
+        "sortBackwards": false,
+        "sortType": "noteFld",
+        "timeLim": 0
+    }',
+    '{}',
+    '{
+        "1": {
+            "collapsed": false,
+            "conf": 1,
+            "desc": "",
+            "dyn": 0,
+            "extendNew": 10,
+            "extendRev": 50,
+            "id": 1,
+            "lrnToday": [
+                0,
+                0
+            ],
+            "mod": 1425279151,
+            "name": "Default",
+            "newToday": [
+                0,
+                0
+            ],
+            "revToday": [
+                0,
+                0
+            ],
+            "timeToday": [
+                0,
+                0
+            ],
+            "usn": 0
+        }
+    }',
+    '{
+        "1": {
+            "autoplay": true,
+            "id": 1,
+            "lapse": {
+                "delays": [
+                    10
+                ],
+                "leechAction": 0,
+                "leechFails": 8,
+                "minInt": 1,
+                "mult": 0
+            },
+            "maxTaken": 60,
+            "mod": 0,
+            "name": "Default",
+            "new": {
+                "bury": true,
+                "delays": [
+                    1,
+                    10
+                ],
+                "initialFactor": 2500,
+                "ints": [
+                    1,
+                    4,
+                    7
+                ],
+                "order": 1,
+                "perDay": 20,
+                "separate": true
+            },
+            "replayq": true,
+            "rev": {
+                "bury": true,
+                "ease4": 1.3,
+                "fuzz": 0.05,
+                "ivlFct": 1,
+                "maxIvl": 36500,
+                "minSpace": 1,
+                "perDay": 100
+            },
+            "timer": 0,
+            "usn": 0
+        }
+    }',
+    '{}'
+);
+`;
